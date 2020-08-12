c5e1062d5138b55092f118f843eea6b0d4b5f950
William Moreschi <williammoreschi@gmail.com>
2020-08-11 22:58:16 -0300

    Criado o Header do Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
-import { View, Button } from 'react-native';
-import { useAuth } from '../../hooks/AuthContext';
+import React, { useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { useAuth, } from '../../hooks/AuthContext';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { Container, Header, HeaderTitle, UserName, ProfileButton, UserAvatar, ProfileIcon } from './styles';
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { signOut, user} = useAuth();
+  const { navigate } = useNavigation();
+  const navigateToProfile = useCallback(()=>{
+    navigate('Profile');
+  },[navigate]);
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title="Sair" onPress={signOut} />
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {"\n"}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{uri: user.avatar_url}} />
+          <ProfileIcon>
+            <Icon name="more-horizontal" size={14} color="#28262e" />
+          </ProfileIcon>
+        </ProfileButton>
+      </Header>
+    </Container>
   );
 };
 
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,46 @@
+import styled from "styled-components/native";
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+export const Header = styled.View`
+  padding: 24px;
+  padding-top: ${getStatusBarHeight() + 24}px;
+  background: #28262e;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const HeaderTitle = styled.Text`
+  color: #f4ede8;
+  font-size: 20px;
+  font-family: 'RobotoSlab-Regular';
+`;
+export const UserName = styled.Text`
+  color: #ff9000;
+  font-family: 'RobotoSlab-Medium';
+`;
+
+export const ProfileButton = styled.TouchableOpacity`
+`;
+
+export const UserAvatar = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
+  background: #efefef;
+`;
+
+export const ProfileIcon = styled.View`
+  position: absolute;
+  right: -2px;
+  bottom: -2px;
+  width: 20px;
+  height: 20px;
+  background: #ffffff;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+`;
